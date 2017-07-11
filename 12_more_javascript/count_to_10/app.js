702f6d154404dbaaf8de544e685421bff544df4c
chrislee1225 <chris.lee98@aol.com>
2017-07-11 19:52:40 -0400

    more arrays

--- 12_more_javascript/count_to_10/app.js
+++ 12_more_javascript/count_to_10/app.js
@@ -1,8 +1,36 @@
+// COUNT TO 10
 console.log("Count to 10");
 
 //  number++ is the same as number = number + 1
 
 for( var number = 1; number <= 10; number++) {
+  console.log(number);
+}
+
+console.log("Finished!");
+
+// COUNT TO 20
+console.log("Count to 20");
+
+for( var number = 1; number <= 20; number++) {
+  console.log(number);
+}
+
+console.log("Finished!");
+
+//COUNT EVEN NUMBERS TO 50
+console.log("Count even numbers to 50");
+
+for(var number = 2; number <= 50; number = number + 2) {
+  console.log(number);
+}
+
+console.log("Finished!");
+
+//COUNT DOWN FROM 10
+console.log("Count down from 10");
+
+for(var number = 10; number >= 1; number = number - 1) {
   console.log(number);
 }
 
