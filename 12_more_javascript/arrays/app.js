caf57d32e5c6f19ea15e7837e877679a5e39cbc5
chrislee1225 <chris.lee98@aol.com>
2017-07-11 19:16:35 -0400

    arrays

---
+++ 12_more_javascript/arrays/app.js
@@ -0,0 +1,16 @@
+var colors = ["black", "blue", "olive"];
+//              0        1        2
+
+// Print the first color
+console.log(colors[0]);
+
+// Print the last color
+console.log(colors[2]);
+
+// Print how many colors are in the array
+console.log(colors.length);
+
+// Print a sentence for each color in your array
+console.log("I like the color " + colors[0]);
+console.log("I like the color " + colors[1]);
+console.log("I like the color " + colors[2]);
