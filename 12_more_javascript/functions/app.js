6b14d193fb36699c8065ed4c01e8022df6995704
chrislee1225 <chris.lee98@aol.com>
2017-07-11 21:20:25 -0400

    more functions

---
+++ 12_more_javascript/functions/app.js
@@ -0,0 +1,26 @@
+var expressGratitude = function(name) {
+  console.log("You have saved our lives, " + name);
+  console.log("We are eternally grateful.");
+};
+
+var join = function(firstWord, secondWord) {
+  var joinedWord = firstWord + " " + secondWord;
+
+  return joinedWord;
+};
+
+var greeting = join("Hello", "there");
+
+console.log(greeting);
+
+var sumOfThree = function(firstNumber, secondNumber, thirdNumber) {
+  var sum = firstNumber + secondNumber + thirdNumber;
+
+  return sum;
+};
+
+var productOfThree = function(firstNumber, secondNumber, thirdNumber) {
+  var product = firstNumber * secondNumber * thirdNumber;
+
+  return product;
+};
