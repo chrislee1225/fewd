e08476989c20f2d30cea49ac545cc9ad3c07ccf2
chrislee1225 <chris.lee98@aol.com>
2017-07-25 21:28:05 -0400

    java

---
+++ 16_lab_city/js/app.js
@@ -0,0 +1,44 @@
+console.log("Loaded bro");
+
+$(".new-tweed-button").click(function() {
+  var userInput = $(".new-tweed-text").val();
+
+  //check for empty string
+  if (userInput) {
+    createTweed(userInput);
+  }
+
+  $(".new-tweed-text").val("");
+});
+
+var createTweed = function(text) {
+
+  var tweed = $("<div>");
+  var tweedText = $("<p>");
+  var tweedDelete = $("<span>");
+
+  tweed.addClass("tweed");
+  tweedText.addClass("tweed-text");
+  tweedDelete.addClass("delete-tweed");
+
+  tweedText.html(text);
+  tweedDelete.html("&times;");
+
+  tweed.append(tweedText).append(tweedDelete);
+
+  tweed.prependTo(".tweeds");
+
+  $(".new-tweed-text").val("");
+};
+
+$(".new-tweed-text").keypress(function(event) {
+  var userInput = $(".new-tweed-text").val();
+
+  if (userInput.length > 140) {
+    event.preventDefault();
+  }
+});
+
+$(".navigation-icon").click(function() {
+  $("nav").toggleClass("navigation-enabled");
+})
